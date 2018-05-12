4fcd78df51ffe21d57a483a19050b980564fb005
mcharron90 <monster.egg2000@gmail.com>
2018-05-12 14:18:30 -0700

    Initial Commit base64

---
+++ cryptopals_all/base64.c
@@ -0,0 +1,9 @@
+//
+//  base64.c
+//  cryptopals_all
+//
+//  Created by Matthew Charron on 5/12/18.
+//  Copyright © 2018 Matthew Charron. All rights reserved.
+//
+
+#include "base64.h"
---
+++ cryptopals_all/base64.h
@@ -0,0 +1,14 @@
+//
+//  base64.h
+//  cryptopals_all
+//
+//  Created by Matthew Charron on 5/12/18.
+//  Copyright © 2018 Matthew Charron. All rights reserved.
+//
+
+#ifndef base64_h
+#define base64_h
+
+#include <stdio.h>
+
+#endif /* base64_h */
